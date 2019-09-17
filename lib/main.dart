87da323ef7cbbbf3f89bace058700aa99a70d57e
Nguyen Nguyen <ntk.nguyen@outlook.com>
2019-09-17 10:34:09 -0500

    Add a stateless and a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Ball(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask me anything'),
+        backgroundColor: Colors.blue.shade500,
+      ),
+      body: Ball(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
