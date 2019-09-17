e87c50f4f831e73affd87d4ec3336706b02ef13c
Nguyen Nguyen <ntk.nguyen@outlook.com>
2019-09-17 14:20:45 -0500

    Add a stateless and a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: Ball(),
+        home: BallPage(),
       ),
     );
 
@@ -26,10 +27,19 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Center(
+      child: Container(
+        child: FlatButton(
+            onPressed: (){
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png')),
+      ),
     );
   }
 }
